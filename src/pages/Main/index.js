2dc5b2987f91ff28c154c19ae918e32e8ed0ca58
Alcirleia de Souza Barbosa <cissa.pmro@gmail.com>
2024-01-23 21:44:03 -0300

    Segundo commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,126 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Container, DeleteButton, Form, List, SubmitButton } from './styles';
+import { FaBars, FaGithub, FaPlus, FaSpinner, FaTrash } from 'react-icons/fa';
+import api from '../../services/api';
 
 const Main = () => {
+
+const [novoRepositorio, setNovoRepositorio] = useState(''); //nome do novo repositório adicionado.
+const [repositorios, setRepositorios] = useState([]); //lista de repositórios
+const [loading, setLoading] = useState(false); //controlar o loading
+const [alert, setAlert] = useState(null);
+
+//Buscar
+useEffect(() => {
+    const repoStorage = localStorage.getItem('item');
+    if(repoStorage) {
+        setRepositorios(JSON.parse(repoStorage)); //passa para array
+    }
+}, []);
+
+//salvar as alterações - atualiza a página 
+useEffect(() => {
+    localStorage.setItem('item', JSON.stringify(repositorios)) //passa para string
+}, [repositorios]);
+
+
+//useCallback é útil quando você precisa passar uma função para um componente filho, 
+//mas não quer que essa função seja recriada desnecessariamente a cada renderização do componente pai.
+const handleSubmit = useCallback((e) => {
+    e.preventDefault(); //Previne o comportamento padrão do formulário (recarregar a página).
+    setAlert(null);
+
+    //função responsável por fazer a requisição
+    const submit = async () => {
+        setLoading(true); //quando clicar no botão de submit vai ficar true
+        setAlert(null);
+        try {
+
+            //verifica se o campo está vazio
+            if(novoRepositorio === ''){
+                throw new Error('Você precisa preencher o campo!')
+            }
+
+            const response = await api.get(`repos/${novoRepositorio}`);
+
+            //verifica se já existe aquele repositório
+            const hasRepo = (repositorios.find(item => item.nome === novoRepositorio));
+            if(hasRepo) {
+                throw new Error('Este repositório já existe');
+            }
+
+           // console.log('Response', response);
+            //pega os dados da api propriedade full_name
+            const data = {
+                nome: response.data.full_name,
+                
+            }
+            console.log('Data', data);
+            //preencheu o array com os dados
+            setRepositorios([...repositorios, data]);
+            setNovoRepositorio('');
+        } catch(error) {
+            setAlert(true);
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    submit();
+}, [novoRepositorio, repositorios]); //quando forem atualizados, vai chamar o useCallback
+
+//Ao acessar o campo
+const handleInputChange = (e) => {
+setNovoRepositorio(e.target.value);
+setAlert(null);
+}
+
+const handleDelete = useCallback((item) => {
+    //retorna tudo para a const procura menos o que foi selecionado.
+    const procura = repositorios.filter(r => r.nome !== item);
+    setRepositorios(procura);
+}, [repositorios]);
+
     return(
         <>
-           <Title>
-                Main
-           </Title>
+           <Container>
+                <h1>
+                    <FaGithub size={25} />
+                    Meus Repositórios</h1>
+                  {/*}  props transitórias prefixadas com $, 
+                  para evitar avisos no console sobre props desconhecidas..*/}
+            <Form onSubmit={handleSubmit} $error={alert ? 'true' : 'false'}> {/*Isso permite que o componente Form saiba se um alerta deve ser exibido ou não.*/}
+                <input type="text" 
+                placeholder="Digite o repositório" 
+                value={novoRepositorio}
+               // onChange={(e) => setNovoRepositorio((e).target.value)}
+               onChange={handleInputChange}
+                />
+              
+                <SubmitButton 
+                loading={loading ? 1 : 0}>  {/*Passando uma props*/}
+                {loading ? (
+                    <FaSpinner color="fff" size={14} />
+                ) : 
+                <FaPlus color="#fff" size={14} />
+                }
+                </SubmitButton>
+            </Form>  
+            <List>
+                {repositorios.map(item => (
+                    <li key={item.nome}>
+                        <span>
+                            <DeleteButton onClick={() => handleDelete(item.nome)}>
+                                <FaTrash size={14} />
+                            </DeleteButton>
+                            {item.nome}</span>
+                        <a href="https://www.google.com"> 
+                            <FaBars size={14} />
+                    </a>
+                    </li>
+                ))}
+            </List>  
+           </Container>
         </>
     )
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,109 @@
-import { styled } from 'styled-components';
+import { css, keyframes, styled } from 'styled-components';
 
-export const Title = styled.div`
-color: #FF0000;
+export const Container = styled.div`
+background: #ffffff;
+max-width: 700px;
+border-radius: 4px;
+padding: 30px;
+margin: 80px auto;
+box-shadow: 0, 0, 20px rgba(0,0,0, 0.2);
 
+h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+svg {
+    margin-right: 10px;
+    }
+}
+
+`;
+
+export const Form = styled.form`
+ margin-top: 30px;
+ display: flex;
+ flex-direction: row;
+
+ input {
+    flex: 1;
+    border: 2px solid ${props => (props.$error === 'true' ? '#ff0000' : '#eee')};
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 17px; 
+ }
+
+`;
+
+//Animação do botão
+const animar = keyframes`
+from {
+    transform: rotate(0deg);
+}
+to {
+    transform: rotate(360deg);
+}
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading,
+}))`
+  background: #0D2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+justify-content: center;
+align-items: center;
+
+&[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+}
+
+${props => props.loading && 
+css`
+ svg {
+    animation: ${animar} 2s linear infinite;
+ }
+`
+}
+`;
+
+export const List = styled.ul`
+ list-style: none;
+ margin-top: 20px;
+
+li {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 0; {/*superior e inferior*/}
+
+    & + li {
+        border-top: 1px solid red;
+    }
+
+    a {
+        color: orange;
+        text-decoration: none; {/*remove o sublinhado*/}
+    }
+
+}
+
+`;
+
+export const DeleteButton = styled.button.attrs({
+    type: 'button'
+})`
+background: transparent;
+border: 0;
+color: orange;
+padding: 8px 7px;
+outline: 0; //exclui a borda de contorno do elemento
+border-radius: 5px;
 `;
